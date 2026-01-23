d0d0fda67ab69a2a4d2adcde20a481c73ec8123e
MaxTB42 <mhvt202@exeter.ac.uk>
2026-01-23 13:10:56 +0000

    Adding dictonary and cleaning up code - MTB

--- file_input.py
+++ file_input.py
@@ -3,7 +3,7 @@ def file_input(file_name):
     open_file = open(file_name, 'r')
     activities = []
     count = 0
-    for line in open(file_name, 'r'):
+    for line in open_file:
         if count == 0:
             num_act = int(line)
         elif count == 1:
@@ -12,11 +12,17 @@ def file_input(file_name):
             activities.append(line)
         count += 1
 
+    time_av = time_budget.split(" ")[0]
+    budget_av = time_budget.split(" ")[1]
 
     print(f"Num of activities: {num_act} ")
-    print(f"Time: {time_budget.split(" ")[0]} ")
-    print(f"Budget: {time_budget.split(" ")[1]}")
-    print(f"Activities: {activities}")
+    print(f"Time: {time_av} ")
+    print(f"Budget: {budget_av}")
+    activ_dict = {0:int(num_act), 1:int(time_av), 2:int(budget_av)}
 
+    for act in enumerate(activities):
+        activ_dict[act[0]+3] = str(act[1].strip())
 
-file_input("Sample Input Files/input_10.txt")
+    return activ_dict
+
+print(file_input("Sample Input Files/input_10.txt"))
