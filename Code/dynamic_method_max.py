16007d99b48bc853151300477a3501418367eca3
MaxTB42 <mhvt202@exeter.ac.uk>
2026-02-03 20:55:19 +0000

    First version of dynamic_method_max.py

--- Code/dynamic_method_max.py
+++ Code/dynamic_method_max.py
@@ -1,3 +1,28 @@
+import file_input_new as fi
+import time
 
+def dynamic_subsets(list):
+    num_activities = list[0][0]
+    av_budget = list[0][2]
+    av_time = list[0][1]
+    total_subsets = 2**num_activities
 
-def dynamic_subsets():
+    subsets = [[]]
+
+    for act in list[1:]:
+        act_cost = act[2]
+        inner_set = []
+
+        for sub in subsets:
+            current_sum = sum(item[2] for item in sub)
+            if current_sum + act_cost <= av_budget:
+                entry = sub + [act]
+                inner_set.append(entry)
+
+        subsets.extend(inner_set)
+    return subsets
+
+start = time.time()
+print(len(dynamic_subsets(fi.file_input("../Input_Files/input_large.txt"))))
+end = time.time()
+print(end - start)
