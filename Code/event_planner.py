c2e9cce4c887c3767d80ad29bf74f278409579ab
Cameron <chrussellpredator07@gmail.com>
2026-02-10 18:24:05 +0000

    Worked out how to take in command line arguments!

--- Code/event_planner.py
+++ Code/event_planner.py
@@ -4,7 +4,15 @@ It uses the two algorihtms to produce the optimal subset of activities.
 It then produces a human readable output of the optimal solution.
 """
 
+# Used for the command line argument "input_file_name"
+import sys
+# Used to access the input files in the Input_Files directory
+from pathlib import Path
+
 # Output the title
 print("========================================")
 print("Event Planner - Results")
 print("========================================")
+
+# Extract the file name
+input_file_name = sys.argv[1]
