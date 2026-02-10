99dd4eda9d565e665f85e92efed713839ee3fda6
Cameron <chrussellpredator07@gmail.com>
2026-02-10 16:18:50 +0000

    Changed main enrty point from main.py to event_planner.py.

---
+++ Code/event_planner.py
@@ -0,0 +1,10 @@
+"""
+This is the main entry point for the coursework.
+It uses the two algorihtms to produce the optimal subset of activities.
+It then produces a human readable output of the optimal solution.
+"""
+
+# Output the title
+print("========================================")
+print("Event Planner - Results")
+print("========================================")
