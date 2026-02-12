806f97080a6174e5cb4d2bb811c47d25d16fc276
MaxTB42 <mhvt202@exeter.ac.uk>
2026-02-12 13:17:58 +0000

    Adding extension DP file

--- Code/event_planner_dp.py
+++ Code/event_planner_dp.py
@@ -1,9 +1,20 @@
 import time
 import dynamic_method as dm
+import dynamic_method_extension as dms
+
 
 start = time.time()
 file = dm.file_to_list('../Input_Files/input_large.txt')
 optimal_subsets = dm.dynamic_subsets(file)
+
+print(f"Length of subset: {len(optimal_subsets)}")
+print(f"Selected activities: {optimal_subsets}")
+end = time.time()
+print("Time taken:", end - start)
+
+start = time.time()
+file = dms.file_to_list('../Input_Files/input_large.txt')
+optimal_subsets = dms.dynamic_subsets(file)
 
 print(f"Length of subset: {len(optimal_subsets)}")
 print(f"Selected activities: {optimal_subsets}")
