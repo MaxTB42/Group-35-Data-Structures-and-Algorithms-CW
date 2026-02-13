a89be160ae09be1810711ab1526e2abd3d3ff880
MaxTB42 <mhvt202@exeter.ac.uk>
2026-02-13 10:37:07 +0000

    Cleaner output using Dictonary

--- Code/dynamic_method.py
+++ Code/dynamic_method.py
@@ -3,7 +3,7 @@ This Python module houses the dynamic programming method to ECM1414 Data Structu
 and Algorithms Coursework using the cost as a limitation.
 """
 
-def dynamic_subsets(activity_list: list[tuple]) -> list[tuple]:
+def dynamic_subsets(activity_list: list[tuple]) -> dict[list[tuple]]:
     """
     This function generates all possible subsets of a given
     list using dynamic programming algorithm.
@@ -12,6 +12,14 @@ def dynamic_subsets(activity_list: list[tuple]) -> list[tuple]:
     av_budget = activity_list[0][2]
     activities = activity_list[1:]
     num_activities = activity_list[0][0]
+
+    # Create a dictionary to hold optimal solution
+    optimal_subset = {
+        'subset': (),
+        'required_time': 0,
+        'required_budget': 0,
+        'total_enjoyment_value': 0
+    }
 
     #Initiates the Dynamic Programming table
     dp_table = [0] * (av_budget + 1)
@@ -25,13 +33,26 @@ def dynamic_subsets(activity_list: list[tuple]) -> list[tuple]:
 
     subsets = []
     budget = av_budget
+    total_time = 0
+    required_budget = 0
+    total_enjoy = 0
 
     #Generates each subset of activities, ensuring budget doesn't exceed
     for i in range(num_activities-1,-1,-1):
         act_cost = activities[i][2]
         act_enjoy = activities[i][3]
+        act_time = activities[i][1]
 
         if budget >= act_cost and dp_table[budget] == dp_table[budget - act_cost] + act_enjoy:
             subsets.append(activities[i])
             budget = budget - act_cost
-    return subsets
+            total_time += act_time
+            required_budget += act_cost
+            total_enjoy += act_enjoy
+
+    optimal_subset['subset'] = subsets
+    optimal_subset['required_time'] = total_time
+    optimal_subset['required_budget'] = required_budget
+    optimal_subset['total_enjoyment_value'] = total_enjoy
+
+    return optimal_subset
--- Code/event_planner_dp.py
+++ Code/event_planner_dp.py
@@ -4,9 +4,9 @@ import file_input_new as fi
 
 
 start = time.time()
-optimal_subsets = dm.dynamic_subsets(fi.file_input('../Input_Files/input_large.txt'))
+optimal_subsets = dm.dynamic_subsets(fi.file_input('../Input_Files/input_1000.txt'))
 
-print(f"Length of subset: {len(optimal_subsets)}")
+print(f"Length of subset: {len(optimal_subsets[0])}")
 print(f"Selected activities: {optimal_subsets}")
 end = time.time()
 print("Time taken:", end - start)
