7e86843c6dcf15c5ba101c199245ed6fddaf03a7
Cameron <chrussellpredator07@gmail.com>
2026-02-19 17:12:06 +0000

    Re-read through file_input.py, updated the module and function doc string and added directory manipulation.

--- Code/file_input.py
+++ Code/file_input.py
@@ -1,27 +1,53 @@
 """
-This module is used to read and parse the file of activities.
+This mobule parses the input file, formatting the activities in
+a way that is suitable for the algorithms.
 """
 
-def file_input(file_name: str) -> tuple[tuple]:
+# Used to exit the program if file doesn't exist
+import sys
+# Used to access the input files in the Input_Files directory
+from pathlib import Path
+
+def file_input(input_file_name: str) -> list[tuple]:
     """
-    This function reads and parses the input file.
+    This is the function that reads and parses the input file.
+
+    ### Parameters:
+     - `input_file_name` -> the name of the requested file as a string.
+    
+    ### Return Data Format:
+     - A list of tupes is returned.
+     - The first tuple contains 3 metadata elements: number of activities, available time, 
+       and available budget.
+     - Every subsequent tuple contains 4 elements about each activity: activity name,
+       time required, budget required, and enjoyment value.
     """
 
-    # Initialising variables
+    # Use Path to construct the path to the input file
+    # Note: cwd = "current working directory"
+    input_file_path = Path.cwd().parent / "Input_Files" / input_file_name
+
+    # If the file doesn't exist, inform the user
+    if Path.exists(input_file_path) is False:
+        print(f"File '{input_file_name}' doesn't exist in path: '{input_file_path}'!")
+        # Exit the program
+        sys.exit()
+
+    # Initialise variables used for file parsing
     activities_from_file = []
-    time_and_budget = ''
+    time_and_budget_line = ''
     number_of_activities = 0
 
     # Read the file contents
-    with open(file_name, 'r', encoding='utf-8') as activity_file:
+    with open(input_file_path, mode='r', encoding='utf-8') as activity_file:
         # Extract the number of activities and time/budget lines
         number_of_activities = int(activity_file.readline())
-        time_and_budget = activity_file.readline()
+        time_and_budget_line = activity_file.readline()
         # Read the rest of the file and split into each activity line
         activities_from_file += activity_file.read().splitlines()
 
     # Extract the time and budget from line 2
-    time_and_budget_separated = time_and_budget.strip().split(' ')
+    time_and_budget_separated = time_and_budget_line.strip().split(' ')
     time_available = time_and_budget_separated[0]
     budget_available = time_and_budget_separated[1]
 
