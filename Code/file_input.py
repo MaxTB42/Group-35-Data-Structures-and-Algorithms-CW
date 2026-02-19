17935cc90f29bf365e8261f0df4b3f88548afbe8
Cameron <chrussellpredator07@gmail.com>
2026-02-19 17:19:13 +0000

    Modified 'file doesn't exist' error message.

--- Code/file_input.py
+++ Code/file_input.py
@@ -29,7 +29,7 @@ def file_input(input_file_name: str) -> list[tuple]:
 
     # If the file doesn't exist, inform the user
     if Path.exists(input_file_path) is False:
-        print(f"File '{input_file_name}' doesn't exist in path: '{input_file_path}'!")
+        print(f"File '{input_file_name}' doesn't exist in the directory: '{input_file_path.parent}'.")
         # Exit the program
         sys.exit()
 
